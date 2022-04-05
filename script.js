91797d87c0ccdc814286613fe2b8a9e68950e063
James Loewen <jamesfloewen@gmail.com>
2022-04-05 10:36:56 -0400

    Set up boilerplate for library project

---
+++ script.js
@@ -0,0 +1,11 @@
+const bookInput = document.querySelector('#book');
+const bookButton = document.querySelector('#bookButton');
+let myLibrary = [];
+
+function Book() {
+  // the constructor...
+}
+
+function addBookToLibrary(book) {
+  myLibrary.push(book);
+}
