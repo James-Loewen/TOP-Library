cf5fb61c7e62ac23c75b129baefb476813b36777
James Loewen <jamesfloewen@gmail.com>
2022-04-05 22:26:43 -0400

    Add a modal for inputting new books
    It isn't functional yet. I need to work out the logic of connecting
    objects to the cards that will display their data. The delete button
    is only semi functioning. It will remove the display, but it won't
    remove the object from the array.

--- script.js
+++ script.js
@@ -1,11 +1,46 @@
-const bookInput = document.querySelector('#book');
-const bookButton = document.querySelector('#bookButton');
-let myLibrary = [];
+const toggleReadStatus = document.querySelectorAll('.read-status');
+const deleteBtns = document.querySelectorAll('.delete-book');
+const newBook = document.querySelector('#add-book');
+const dialog = document.querySelector('#new-book-dialog');
+const submitButton = document.querySelector('#new-book-submit');
+const cancelBtn = document.querySelector('#cancel-input');
+const cardContainer = document.querySelector('.card-container');
+const titleInput = document.querySelector('#title-input');
+const authorInput = document.querySelector('#author-input');
 
-function Book() {
-  // the constructor...
+function Book(title, author) {
+  this.title = title;
+  this.author = author;
 }
 
-function addBookToLibrary(book) {
-  myLibrary.push(book);
-}
+const hobbit = new Book("The Hobbit", "J.R.R. Tolkien");
+
+const myLibrary = [];
+
+myLibrary.push(hobbit);
+
+toggleReadStatus.forEach(btn => {
+  btn.addEventListener('click', (e) => {
+    e.target.classList.toggle('read');
+    if (Array.from(e.target.classList).includes('read')) {
+      e.target.textContent = "Read";
+    } else {
+      e.target.textContent = "Unread";
+    }
+  });
+});
+
+deleteBtns.forEach(btn => {
+  btn.addEventListener('click', (e) => {
+    e.target.parentElement.remove();
+  });
+});
+
+newBook.addEventListener('click', () => dialog.showModal());
+cancelBtn.addEventListener('click', () => dialog.close());
+
+submitButton.addEventListener('click', () => {
+  let newBook = new Book(titleInput.value, authorInput.value);
+  myLibrary.push(newBook);
+  dialog.close()
+});
