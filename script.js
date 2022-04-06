82c08503de97a19c94257250339c100fab5e7c67
James Loewen <jamesfloewen@gmail.com>
2022-04-06 11:58:44 -0400

    Complete functionality for Library app
    Can add new books.
    Can delete books from list.
    Can toggle "read" status of books.
    
    There are some styling changes I would still like to make. To start
    with, I want the "X" button to be in a more appealing location. It would
    also be neat to try and add styling for smaller viewports as well.

--- script.js
+++ script.js
@@ -1,46 +1,97 @@
-const toggleReadStatus = document.querySelectorAll('.read-status');
-const deleteBtns = document.querySelectorAll('.delete-book');
-const newBook = document.querySelector('#add-book');
+function Book(title, author, haveRead = false) {
+  this.title = title;
+  this.author = author;
+  this.haveRead = haveRead
+}
+
+const myLibrary = [];
+
+const hobbit = new Book("The Hobbit", "J.R.R. Tolkien", true);
+const giver = new Book("The Giver", "Lois Lowry", true);
+
+myLibrary.push(hobbit);
+myLibrary.push(giver);
+
 const dialog = document.querySelector('#new-book-dialog');
-const submitButton = document.querySelector('#new-book-submit');
-const cancelBtn = document.querySelector('#cancel-input');
 const cardContainer = document.querySelector('.card-container');
 const titleInput = document.querySelector('#title-input');
 const authorInput = document.querySelector('#author-input');
 
-function Book(title, author) {
-  this.title = title;
-  this.author = author;
+function populateBookList() {
+  cardContainer.innerHTML = '';
+  
+  myLibrary.forEach(book => {
+    let card = document.createElement('div');
+    card.classList.add('card');
+    let xButton = document.createElement('button');
+    xButton.type = 'button';
+    xButton.classList.add('delete-book');
+    xButton.textContent = 'X';
+    let titleText = document.createElement('span');
+    titleText.classList.add('title');
+    titleText.textContent = book.title;
+    let authorText = document.createElement('span');
+    authorText.classList.add('author');
+    authorText.textContent = book.author;
+    let readButton = document.createElement('button');
+    readButton.type = 'button';
+    readButton.classList.add('read-status');
+    if (book.haveRead) {
+      readButton.classList.add('read');
+      readButton.textContent = "Read";
+    } else {
+      readButton.textContent = 'Unread';
+    }
+    card.appendChild(xButton);
+    card.appendChild(titleText);
+    card.appendChild(authorText);
+    card.appendChild(readButton);
+    cardContainer.appendChild(card);
+  });
+  
+  const toggleReadStatus = document.querySelectorAll('.read-status');
+  
+  toggleReadStatus.forEach(btn => {
+    btn.addEventListener('click', (e) => {
+      e.target.classList.toggle('read');
+      let childIndex = Array.from(toggleReadStatus).indexOf(e.target);
+      myLibrary[childIndex].haveRead = !myLibrary[childIndex].haveRead;
+      // console.log(myLibrary[childIndex].haveRead);
+      if (Array.from(e.target.classList).includes('read')) {
+        e.target.textContent = "Read";
+      } else {
+        e.target.textContent = "Unread";
+      }
+    });
+  });
+  
+  const deleteBtns = document.querySelectorAll('.delete-book');
+  
+  deleteBtns.forEach(btn => {
+    btn.addEventListener('click', (e) => {
+      let childIndex = Array.from(deleteBtns).indexOf(e.target);
+      myLibrary.splice(childIndex, 1);
+      console.table(myLibrary);
+      populateBookList();
+    });
+  });
+  
 }
 
-const hobbit = new Book("The Hobbit", "J.R.R. Tolkien");
-
-const myLibrary = [];
-
-myLibrary.push(hobbit);
-
-toggleReadStatus.forEach(btn => {
-  btn.addEventListener('click', (e) => {
-    e.target.classList.toggle('read');
-    if (Array.from(e.target.classList).includes('read')) {
-      e.target.textContent = "Read";
-    } else {
-      e.target.textContent = "Unread";
-    }
-  });
-});
-
-deleteBtns.forEach(btn => {
-  btn.addEventListener('click', (e) => {
-    e.target.parentElement.remove();
-  });
-});
-
+const newBook = document.querySelector('#add-book');
 newBook.addEventListener('click', () => dialog.showModal());
+
+const cancelBtn = document.querySelector('#cancel-input');
 cancelBtn.addEventListener('click', () => dialog.close());
 
+const submitButton = document.querySelector('#new-book-submit');
 submitButton.addEventListener('click', () => {
   let newBook = new Book(titleInput.value, authorInput.value);
   myLibrary.push(newBook);
+  populateBookList();
+  titleInput.value = '';
+  authorInput.value = '';
   dialog.close()
 });
+
+populateBookList();
